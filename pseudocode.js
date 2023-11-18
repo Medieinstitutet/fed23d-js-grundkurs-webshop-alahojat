3a9427c1d770e67d4764f4948e5e5439f6deb01e
Ala Hojat Jalali <alahojat@icloud.com>
2023-11-18 16:06:37 +0100

    add scriptfile and file for pseudocode

---
+++ pseudocode.js
@@ -0,0 +1,44 @@
+// Create array with items
+
+
+// Function: array filters
+    /*Function: sortItemsName (enables items to be filtered based on name)
+    - when button is clicked items in array sort alphabetically
+
+    Function: sortItemsPrice (enables items to be filtered based on price)
+    - when button is clicked items in array sort according to price
+
+    Function: sortItemsCategory (enables items to be filtered based on category)
+    - when button is clicked items in array sort according to category
+
+    Function: sortItemsRating (enables items to be filtered based on rating)
+    when button is clicked items in array sort according to rating
+    */
+
+
+
+// Function: addItem (adds item to cart on start page)
+    /*
+    - when a customer CLICKS on the minus button the value of the input decreases by 1 (number)
+    - IF the value reaches 0 customer the function of the minus button no longer works
+    - when a customer clicks on the plus button the value of the input increases by 1 (number)
+    */
+
+
+
+
+
+// Function: displayShoppingCart (connect and style cart based off values from array with items)
+
+// Change value of cart based on rules... IF..ELSE
+
+
+
+
+
+
+
+
+
+
+
