9fbc4667d8e4d6cb3658277eda94940a6142b629
Ala Hojat Jalali <alahojat@icloud.com>
2023-11-18 21:58:32 +0100

    link: page font

--- pseudocode.js
+++ pseudocode.js
@@ -1,36 +1,75 @@
 // Create array with items
 
-
-// Function: array filters
-    /*Function: sortItemsName (enables items to be filtered based on name)
-    - when button is clicked items in array sort alphabetically
-
-    Function: sortItemsPrice (enables items to be filtered based on price)
-    - when button is clicked items in array sort according to price
-
-    Function: sortItemsCategory (enables items to be filtered based on category)
-    - when button is clicked items in array sort according to category
-
-    Function: sortItemsRating (enables items to be filtered based on rating)
-    when button is clicked items in array sort according to rating
+/* Function: addItem (adds item to cart on start page)
+    - when the minus-button is clicked the value of the input decreases by 1 (number)
+    - A total next to the cart is updated
+    - IF the value reaches 0 customer the function of the minus-button no longer works
+    - when the plus-button is clicked the value of the input increases by 1 (number)
     */
 
 
-
-// Function: addItem (adds item to cart on start page)
-    /*
-    - when a customer CLICKS on the minus button the value of the input decreases by 1 (number)
-    - IF the value reaches 0 customer the function of the minus button no longer works
-    - when a customer clicks on the plus button the value of the input increases by 1 (number)
-    */
-
-
-
+/* Function: change value of cart based on rules... IF..ELSE
+    
+*/
 
 
 // Function: displayShoppingCart (connect and style cart based off values from array with items)
 
-// Change value of cart based on rules... IF..ELSE
+
+
+/* Function: array filters
+    Function: sortItemsName (enables items to be filtered based on name)
+    - when button is clicked, items in array sort alphabetically
+
+    Function: sortItemsPrice (enables items to be filtered based on price)
+    - when button is clicked, items in array sort according to price
+
+    Function: sortItemsCategory (enables items to be filtered based on category)
+    - when button is clicked, items in array sort according to category
+
+    Function: sortItemsRating (enables items to be filtered based on rating)
+    when button is clicked, items in array sort according to rating
+*/
+
+
+
+
+/* Function: toggleView (toggles between the shopview and the cartview)
+    - when cart icon is clicked, the order page appears
+    - when cart icon is clicked again, the order page disappears
+*/
+
+/* Function: orderSummary (show an overview of items displayShoppingCart in the order summary) 
+    - when 
+        - IF order placed before 10am on a monday, 10% off is applied on full amount of the cart
+        - order summary shows "Måndagsrabatt: 10 % på hela beställningen"
+*/
+
+/* Function: removeItem (removes item from order summary)
+    - when button is clicked, value input resets to 0
+*/
+
+/* Function: paymentCard (activates payment form)
+    - when card-button is clicked, new inputfields for cardnumber, date/year and CVC appear
+*/
+
+/* Function: paymentInvoice (activats invoice form)
+    - when invoice-button is clicked, new inputfield for swedish ID number appears
+*/
+
+
+/* Function: resetForm (resets both form and orders upon click)
+    - when button is clicked all input values are deleted
+    - card or invoice is also reset
+    */
+
+/* Function: orderButton (enables button to be pressed when form is correctly filled, activates "Thank You" note.
+However resets form after 15 min)
+    - when form input values for order AND invoice are correct, button changes from grey and inactive
+     to blue and clickable
+    - when blue button is clicked a thank you note pops up
+*/
+
 
 
 
